77f7e0e194ee6d2dc6f0ced33d7fb6217542957b
kha333n <44539272+kha333n@users.noreply.github.com>
2019-03-07 18:22:49 +0500

    Add files via upload

---
+++ I love you.cpp
@@ -0,0 +1,14 @@
+// Created by Максим Бурлака
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "I " << "LOVE " << "You""\n";
+    cout << "Do " << "you " << "love " <<"me?";
+    string reply;
+    cin>>reply ;
+    if (reply ="yes" || reply = "Yes" || reply=  "YES")
+    {C}
+    return 0;
+}
